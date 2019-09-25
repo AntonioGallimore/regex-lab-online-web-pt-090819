ffb9f964453f1208871801d5e2548ee8c8367762
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-09-25 01:09:04 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,5 +16,5 @@ text.match(/^[A-Z].*[a-zA-Z][.]$/) ? true : false
 end
 
 def valid_phone_number?(phone)
-phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)[])/)
+phone.match(/([0-9] ?){10}/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7}\b)/) ? true : false
 end
